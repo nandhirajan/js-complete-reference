3be923c1255a8992c3e0aa4c2ef7af0d1124058c
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-07-19 10:47:49 +0530

    Nullish coalescing operator

--- helloworld.js
+++ helloworld.js
@@ -1,9 +1,16 @@
-let age = prompt("How old are you?", 50);
-let isUserConfirmed = confirm(`Are you sure you reached ${age}`);
+// let age = prompt("How old are you?", 50);
+// let isUserConfirmed = confirm(`Are you sure you reached ${age}`);
 
-if (isUserConfirmed) {
-    alert(`You are ${age} year old!`);
-}
-else {
-    alert("You entered incorrect age!");
-}
+// if (isUserConfirmed) {
+//     alert(`You are ${age} year old!`);
+// }
+// else {
+//     alert("You entered incorrect age!");
+// }
+
+let num1 ;
+let num2 = prompt("Input number 2");
+
+// let result = (num1 !== "" && num1 !== null && num1 !== undefined) ? num1 : num2;
+let result = num1 ?? num2;
+alert(`First non null value ${result}`);
