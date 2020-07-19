06a120cf4712be106875415b08c032bef6529b8e
Nandhi Pandiaraj <nandhirajan.pandiaraj@byjus.com>
2020-07-19 10:31:25 +0530

    Working on Interaction: Prompt, Confirm and Alert

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,9 @@
-alert("hello world");
+let age = prompt("How old are you?", 50);
+let isUserConfirmed = confirm(`Are you sure you reached ${age}`);
+
+if (isUserConfirmed) {
+    alert(`You are ${age} year old!`);
+}
+else {
+    alert("You entered incorrect age!");
+}
